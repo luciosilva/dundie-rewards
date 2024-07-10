f48d9dbb42741f71ddc5d6e1655b4195c4a2e053
Lúcio Ferreira da Silva <sandmanlucio@gmail.com>
2024-07-09 22:22:39 -0300

    added gitignore #8 (#11)
    * added gitignore #8
    
    * Set up project initial organization and metadata
    
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__ for package
    
    closes #8
    
    * Fixed newline as requested
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setuptools
+# pyproject
+# external build tools (poetry, flit)
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Lucio Silva",
+#    packages=["dundie"],
+    packages=find_packages(),
+)
