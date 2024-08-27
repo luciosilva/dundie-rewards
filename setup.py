1ed53094bf803cb52eb8a22ef0d2a6772404dd73
Lucio F. da Silva <sandmanlucio@gmail.com>
2024-08-26 21:23:59 -0300

    Add CI GHA

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Lucio Silva",
+    python_requires=">=3.8",
 #    packages=["dundie"],
     packages=find_packages(),
     entry_points = {
