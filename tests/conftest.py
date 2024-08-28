a859230b368c052b8ec5dbfac37c2976772c4962
Lucio F. da Silva <sandmanlucio@gmail.com>
2024-08-28 20:57:37 -0300

    Adding finaliziers, setup and teardown to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 integration: Mark integration tests
 unit: Mark unit tests
@@ -9,3 +11,10 @@ low: Low Priority
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers',line)
+        
+        
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request): # injeção de dependências
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield # protocolo de generators
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,10 +1,52 @@
+import os
+import uuid
 import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
 
+def setup_module():
+    print()
+    print("roda antes dos testes desse module\n")
+
+def teardown_module():
+    print()
+    print("roda depois dos testes desse module\n")
+
+
+#@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("isso é sujeira...")
+    yield
+    file_.remove()
+
+
+
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load(create_new_file, request):
     """Test load function."""
+    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
+    request.addfinalizer(lambda: os.unlink(filepath))
+    
+    if "a" == "a":
+        request.addfinalizer(lambda: print("\nTerminou"))
+        
+    with open(filepath, "w") as file_:
+        file_.write("dados úteis somente para o teste")
+    
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == 'J'
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test load function."""
+    
+    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
+        file_.write("dados úteis somente para o teste")
+    
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
